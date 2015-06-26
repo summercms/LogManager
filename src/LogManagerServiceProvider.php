f81c12839291423f44cbcb30a254a1fd46477dc6
Cristian Tabacitu <cristitabacitu@gmail.com>
2015-06-26 17:02:39 +0300

    barebones skeleton package

---
+++ src/LogManagerServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Dick\LogManager;
+
+use Illuminate\Support\ServiceProvider;
+
+class LogManagerServiceProvider extends ServiceProvider
+{
+    /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register any package services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
